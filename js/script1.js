62ff4c3052ef3bc9373ac050761b819d99cd502e
Gilia Góngora <giliagongora.g@gmail.com>
2024-02-19 16:44:15 -0300

    Solucionando errores de sintaxis en JS

--- js/script1.js
+++ js/script1.js
@@ -13,23 +13,29 @@ function limpiandoDatos() {
 }
 
 //capturamos los datos del formulario de cada ID
-let nombre = document
-  .getElementById("nombre")
-  .addEventListener("input", () =>
-    console.log(document.getElementById("nombre").value)
-  );
+var nombre = document.getElementById("nombre");
+miBtn.addEventListener('click',function(){
+document.getElementById("input").setAttribute('style', 
+'color: red; background-color: #e53e3e; width: 500px; height: 600px;border: 1px solid black');
+});
 
-let asunto = document
-  .getElementById("asunto")
-  .addEventListener("input", () =>
-    console.log(document.getElementById("asunto").value)
-  );
+// let nombre = document
+//   .getElementById("nombre")
+//   .addEventListener("input", () =>
+//     console.log(document.getElementById("nombre").value)
+//   );
 
-let mensaje = document
-  .getElementById("mensaje")
-  .addEventListener("input", () =>
-    console.log(document.getElementById("mensaje").value)
-  );
+// let asunto = document
+//   .getElementById("asunto")
+//   .addEventListener("input", () =>
+//     console.log(document.getElementById("asunto").value)
+//   );
+
+// let mensaje = document
+//   .getElementById("mensaje")
+//   .addEventListener("input", () =>
+//     console.log(document.getElementById("mensaje").value)
+//   );
 
 
 // Campos vacios

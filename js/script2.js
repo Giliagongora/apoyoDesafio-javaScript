461895a2b296416ec7272c303c3fd683033f8565
Gilia Góngora <giliagongora.g@gmail.com>
2024-02-19 16:11:01 -0300

    Añadiendo funcionalidad de cambio de color segun seleccion

--- js/script2.js
+++ js/script2.js
@@ -1,51 +1,47 @@
+var miBtn = document.getElementById("btn-1");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
 
-// // Declaro una función que cambia el color de fondo de un elemento
-// function cambiarColor(idElemento, color) {
-//   // Obtengo el elemento por su id
-//   let elemento = document.getElementById("btn-1");
-//   // Cambio el color de fondo del elemento
-//   elemento.style.backgroundColor = color;
-// }
-
-// // Obtengo el elemento que quiero presionar por su id
-// let boton = document.getElementById("btn-1");
-// console.log(boton);
-
-// // Asigno la función cambiarColor al evento onclick del elemento
-// // Le paso como argumentos el id del elemento que quiero cambiar y el color que quiero poner
-// boton.onclick = function () {
-//   cambiarColor("caja", "red");
-// };
-
-let caja = "caja";
-
-//capturo el boton
-var btnColorRed = console.log( document.getElementById("btn-1"));
-
-
-
-btnColorRed.addEventListener("click", function () {
-    document.getElementById("caja").setAttribute('style', 
-'color: red');
-
-
-            // From the drop-down menu, obtain 
-            // the value of the chosen color.
-            var selectedColor =
-            selectColor.options[selectColor.selectedIndex].value;
- 
-            // Set the font color of the "gfg" 
-            // element to the selected color
-            gfg.style.color = selectedColor;
-
-// let caja = document
-//   .getElementById("caja")
-//   .addEventListener("button", () =>
-//     console.log(document.getElementById("caja").value)
-//   );
-
-//   alert("cacha");
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #e53e3e; width: 500px; height: 600px;border: 1px solid black');
 });
 
-console.log(btnColorRed);
-console.log(caja);
+
+
+var miBtn = document.getElementById("btn-2");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #dd6b20; width: 500px; height: 600px;border: 1px solid black');
+});
+
+
+
+var miBtn = document.getElementById("btn-3");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
+
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #faf089; width: 500px; height: 600px;border: 1px solid black');
+});
+
+var miBtn = document.getElementById("btn-4");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #48bb78; width: 500px; height: 600px;border: 1px solid black');
+});
+
+var miBtn = document.getElementById("btn-5");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #81e6d9; width: 500px; height: 600px;border: 1px solid black');
+});
+
+var miBtn = document.getElementById("btn-6");
+miBtn.addEventListener('click',function(){
+//cuerpo de la función anónima que se ejecuta al hacer click
+document.getElementById("caja").setAttribute('style', 
+'color: red; background-color: #d53f8c; width: 500px; height: 600px;border: 1px solid black');
+});

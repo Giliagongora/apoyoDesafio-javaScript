a765ccf8c1d7442f8586dd3102d359d3054f31f9
Gilia Góngora <giliagongora.g@gmail.com>
2024-04-25 17:45:26 -0400

    Cambio en validaciones

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,35 +1,35 @@
-let miBtn1 = document.getElementById("btn-1");
-miBtn1.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #e53e3e; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn1 = document.getElementById("btn-1");
+// miBtn1.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #e53e3e; width: 500px; height: 600px;border: 1px solid black');
+// });
 
-let miBtn2 = document.getElementById("btn-2");
-miBtn2.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #dd6b20; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn2 = document.getElementById("btn-2");
+// miBtn2.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #dd6b20; width: 500px; height: 600px;border: 1px solid black');
+// });
 
-let miBtn3 = document.getElementById("btn-3");
-miBtn3.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #faf089; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn3 = document.getElementById("btn-3");
+// miBtn3.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #faf089; width: 500px; height: 600px;border: 1px solid black');
+// });
 
-let miBtn4 = document.getElementById("btn-4");
-miBtn4.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #48bb78; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn4 = document.getElementById("btn-4");
+// miBtn4.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #48bb78; width: 500px; height: 600px;border: 1px solid black');
+// });
 
-let miBtn5 = document.getElementById("btn-5");
-miBtn5.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #81e6d9; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn5 = document.getElementById("btn-5");
+// miBtn5.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #81e6d9; width: 500px; height: 600px;border: 1px solid black');
+// });
 
-let miBtn6 = document.getElementById("btn-6");
-miBtn6.addEventListener('click',function(){
-document.getElementById("caja").setAttribute('style', 
-'color: red; background-color: #d53f8c; width: 500px; height: 600px;border: 1px solid black');
-});
+// let miBtn6 = document.getElementById("btn-6");
+// miBtn6.addEventListener('click',function(){
+// document.getElementById("caja").setAttribute('style', 
+// 'color: red; background-color: #d53f8c; width: 500px; height: 600px;border: 1px solid black');
+// });
